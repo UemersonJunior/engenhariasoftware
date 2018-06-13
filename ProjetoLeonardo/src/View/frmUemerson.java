3022270840088d090e30df393fdcddf3bd6b5f28
Developer <Developer@Gustavo>
2018-06-12 22:19:40 -0300

    Versão Taynan

--- ProjetoLeonardo/src/View/frmUemerson.java
+++ ProjetoLeonardo/src/View/frmUemerson.java
@@ -17,6 +17,8 @@ public class frmUemerson extends JFrame {
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
 
 	/**
 	 * Launch the application.
@@ -71,5 +73,23 @@ public class frmUemerson extends JFrame {
 		textField_1.setBounds(86, 54, 86, 20);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Taynan");
+		lblNewLabel.setBounds(30, 89, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Taynan");
+		lblNewLabel_1.setBounds(30, 126, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(86, 86, 86, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(86, 123, 86, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
 	}
 }
